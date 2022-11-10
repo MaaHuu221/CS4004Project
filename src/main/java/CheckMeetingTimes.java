ea86f54e6eaee2ecd6e5b64547a4d4d8f199a65e
caoimhe <caoimh3cahill@gmail.com>
2022-11-10 13:54:05 +0000

    created code to make test work

--- src/main/java/CheckMeetingTimes.java
+++ src/main/java/CheckMeetingTimes.java
@@ -1,3 +1,17 @@
+import java.util.ArrayList;
+
 public class CheckMeetingTimes {
+
+
+        public boolean CheckMeetingTimes(ArrayList<Double> list, Double time) {
+            if (list.contains(time)) {
+                return false;
+            } else {
+                list.add(time);
+                return true;
+            }
+        }
+
+
 
 }
