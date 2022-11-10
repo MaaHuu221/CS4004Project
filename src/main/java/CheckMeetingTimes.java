44e25556236351131f6c49af2e9423952406c70a
caoimhe <caoimh3cahill@gmail.com>
2022-11-10 14:03:03 +0000

    created code to make test work

--- src/main/java/CheckMeetingTimes.java
+++ src/main/java/CheckMeetingTimes.java
@@ -5,9 +5,11 @@ public class CheckMeetingTimes {
 
         public boolean CheckMeetingTimes(ArrayList<Double> list, Double time) {
             if (list.contains(time)) {
+                System.out.println("Participant already has a meeting scheduled at this time.");
                 return false;
             } else {
                 list.add(time);
+                System.out.println("Meeting at " + time + " has been added to this participants schedule. The participant will review this and either accept or decline");
                 return true;
             }
         }
