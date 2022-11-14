e11fcaaeb9422a1ef541b2bd61ea0781c95f19e3
caoimhe <caoimh3cahill@gmail.com>
2022-11-14 12:57:51 +0000

    creating tests and code tested

--- src/main/java/TimeFormat.java
+++ src/main/java/TimeFormat.java
@@ -1,2 +1,12 @@
+import java.util.regex.Pattern;
+
 public class TimeFormat {
+
+    public static final Pattern TIME_PATTERN = Pattern.compile(
+            "(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]"
+    );
+
+    public static boolean IsTimeValid(CharSequence time) {
+        return time != null && TIME_PATTERN.matcher(time).matches();
+    }
 }
--- src/test/java/TimeFormatTest.java
+++ src/test/java/TimeFormatTest.java
@@ -1,4 +1,6 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -6,6 +8,54 @@ class TimeFormatTest {
 
     @BeforeEach
     void setUp() {
+    }
+
+
+    @Test
+    void testingForCorrectFormatHoursAndMinutes(){
+        Assertions.assertTrue(TimeFormat.IsTimeValid("12:30"));
+    }
+
+    @Test
+    void testingForInvalidMinute(){
+        Assertions.assertFalse(TimeFormat.IsTimeValid("12:65"));
+    }
+
+    @Test
+    void testingForInvalidHour(){
+        Assertions.assertFalse(TimeFormat.IsTimeValid("26:30"));
+    }
+
+    @Test
+    void testingForIncorrectHourFormat(){Assertions.assertFalse(TimeFormat.IsTimeValid("123:30"));}
+
+    @Test
+    void testingForIncorrectMinuteFormat(){Assertions.assertFalse(TimeFormat.IsTimeValid("12:300"));}
+
+    @Test
+    void testingForSmallHourFormat(){Assertions.assertFalse(TimeFormat.IsTimeValid("1:30"));}
+
+    @Test
+    void testingForSmallMinuteFormat(){Assertions.assertFalse(TimeFormat.IsTimeValid("14:2"));}
+
+    @Test
+    void testingForInvalidCharacter(){
+            Assertions.assertFalse(TimeFormat.IsTimeValid("12:30!"));
+    }
+
+    @Test
+    void testingForDecimalInHour(){
+        Assertions.assertFalse(TimeFormat.IsTimeValid("12.5:30"));
+    }
+
+    @Test
+    void testingForDecimalInMinute(){
+        Assertions.assertFalse(TimeFormat.IsTimeValid("12:30.6"));
+    }
+
+    @Test
+    void testingForSeconds(){
+        Assertions.assertFalse(TimeFormat.IsTimeValid("12:30:45"));
     }
 
 }
