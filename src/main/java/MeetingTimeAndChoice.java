423341edeab9be3aff1d6fe8a249a936d7e7249f
caoimhe <caoimh3cahill@gmail.com>
2022-11-14 11:59:55 +0000

    creating tests and then class. Parameterized tests

--- src/main/java/MeetingTimeAndChoice.java
+++ src/main/java/MeetingTimeAndChoice.java
@@ -1,2 +1,26 @@
+import java.util.ArrayList;
+
 public class MeetingTimeAndChoice {
+
+    public boolean MeetingTimeAndChoice(ArrayList<String> list, String time, String choice){
+        if (list.isEmpty()) {
+            list.add(time);
+            return true;
+        } else {
+            if (list.contains(time)) {
+                System.out.println("Participant already has a meeting scheduled at this time.");
+                return false;
+            } else {
+                if (choice.equals("yes")) {
+                    list.add(time);
+                    System.out.println("Meeting at " + time + " has been added to this participants schedule. The participant will review this and either accept or decline");
+                    return true;
+                } else if (choice.equals("no")) {
+                    return false;
+                }
+            }
+
+        }
+        return false;
+    }
 }
--- src/test/java/MeetingTimeAndChoiceTest.java
+++ src/test/java/MeetingTimeAndChoiceTest.java
@@ -1,10 +1,40 @@
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MeetingTimeAndChoiceTest {
 
+    MeetingTimeAndChoice overlapping;
+
     @BeforeEach
     void setUp() {
+        overlapping = new MeetingTimeAndChoice();
+    }
+
+    @ParameterizedTest
+    @CsvSource({"11:00 to 12:00,yes,true",
+            "15:30 to 16:00,x,false,",
+            "11:00 to 12:00,no,false",
+            "0,x,false"})
+    void overlappingTime(String time, String choice, boolean expected) {
+        ArrayList<String> times = new ArrayList<>(Arrays.asList("12:30 to 13:30", "15:30 to 16:00"));
+        boolean result = overlapping.MeetingTimeAndChoice(times, time, choice);
+        assertEquals(expected, result);
+
+
+    }
+
+    @Test
+    void nullList(){
+        ArrayList<String> times = new ArrayList<>();
+        assertTrue(overlapping.MeetingTimeAndChoice(times, "11:00", "x"));
     }
 }
+
+
