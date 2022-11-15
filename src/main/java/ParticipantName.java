5f4b56dc557570d9332fa8004ba3138f085e8de2
caoimhe <caoimh3cahill@gmail.com>
2022-11-15 19:52:24 +0000

    finished test

--- src/main/java/ParticipantName.java
+++ src/main/java/ParticipantName.java
@@ -1,2 +1,12 @@
 public class ParticipantName {
+
+    public boolean ParticipantName(String name){
+        String participants = "CaoimheCahill";
+        if(participants.toLowerCase().equals(name.toLowerCase())){
+            return true;
+        }else {
+            return false;
+        }
+
+    }
 }
--- src/test/java/ParticipantNameTest.java
+++ src/test/java/ParticipantNameTest.java
@@ -1,10 +1,31 @@
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ParticipantNameTest {
 
+    ParticipantName participantName;
+
     @BeforeEach
     void setUp() {
+
+        participantName = new ParticipantName();
     }
+
+    @ParameterizedTest(name = "#{index} - Test with {0}, expect {2}")
+    @CsvSource({"CaoimheCahill,true",
+            "caoimhecahill,true",
+            "CaOiMhEcAhIlL,true",
+            "CAOIMHECAHILL,true",
+            "CaoimheCahill1,false",
+            "Caoimhe.Cahill, false",
+            "CaimheCahil,false",})
+    void name(String name, boolean expected){
+        boolean result = participantName.ParticipantName(name);
+        assertEquals(expected, result);
+    }
+
+
 }
