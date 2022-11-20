5317ec27c08ef730aa338549854da3bcacdc69f8
caoimhe <caoimh3cahill@gmail.com>
2022-11-20 14:59:25 +0000

    last minute fix

--- src/test/java/TimeFormatTest.java
+++ src/test/java/TimeFormatTest.java
@@ -8,75 +8,77 @@ import org.junit.jupiter.params.provider.CsvSource;
 import static org.junit.jupiter.api.Assertions.*;
 class TimeFormatTest {
 
+    TimeFormat timeFormat;
+
     @BeforeEach
     void setUp() {
+        timeFormat = new TimeFormat();
     }
 
     @Test
     @DisplayName("Testing Correct format | true")
     void testingForCorrectFormat(){
-        Assertions.assertTrue(TimeFormat.IsTimeValid("12:30"));
+        assertTrue(timeFormat.IsTimeValid("12:30"));
     }
 
     @Test
     @DisplayName("Testing for nonexistent minute | false")
     void testingForNonexistentMinute(){
-        Assertions.assertFalse(TimeFormat.IsTimeValid("12:65"));
+        assertFalse(timeFormat.IsTimeValid("12:65"));
     }
 
     @Test
     @DisplayName("Testing for nonexistent hour | false")
-    void testingForNonexistentHour(){
-        Assertions.assertFalse(TimeFormat.IsTimeValid("26:30"));
+    void testingForNonexistentHour(){ assertFalse(timeFormat.IsTimeValid("26:30"));
     }
 
     @Test
     @DisplayName("Testing for too many hour numbers | false")
-    void testingForLargeHourFormat(){Assertions.assertFalse(TimeFormat.IsTimeValid("123:30"));}
+    void testingForLargeHourFormat(){assertFalse(timeFormat.IsTimeValid("123:30"));}
 
     @Test
     @DisplayName("Testing for too many minute numbers | false")
-    void testingForLargeMinuteFormat(){Assertions.assertFalse(TimeFormat.IsTimeValid("12:300"));}
+    void testingForLargeMinuteFormat(){assertFalse(timeFormat.IsTimeValid("12:300"));}
 
     @Test
     @DisplayName("Testing for not enough hour numbers | false")
-    void testingForSmallHourFormat(){Assertions.assertFalse(TimeFormat.IsTimeValid("1:30"));}
+    void testingForSmallHourFormat(){assertFalse(timeFormat.IsTimeValid("1:30"));}
 
     @Test
     @DisplayName("Testing for not enough minute numbers | false")
-    void testingForSmallMinuteFormat(){Assertions.assertFalse(TimeFormat.IsTimeValid("14:2"));}
+    void testingForSmallMinuteFormat(){assertFalse(timeFormat.IsTimeValid("14:2"));}
 
     @Test
     @DisplayName("Testing for an invalid character | false")
-    void testingForInvalidCharacter(){Assertions.assertFalse(TimeFormat.IsTimeValid("12:30!"));}
+    void testingForInvalidCharacter(){assertFalse(timeFormat.IsTimeValid("12:30!"));}
 
     @Test
     @DisplayName("Testing for a decimal number for hour | false")
     void testingForDecimalInHour(){
-        Assertions.assertFalse(TimeFormat.IsTimeValid("12.5:30"));
+        assertFalse(timeFormat.IsTimeValid("12.5:30"));
     }
 
     @Test
     @DisplayName("Testing for a decimal number for minute | false")
     void testingForDecimalInMinute(){
-        Assertions.assertFalse(TimeFormat.IsTimeValid("12:30.6"));
+        assertFalse(timeFormat.IsTimeValid("12:30.6"));
     }
 
     @Test
     @DisplayName("Testing for seconds | false")
     void testingForSeconds(){
-        Assertions.assertFalse(TimeFormat.IsTimeValid("12:30:45"));
+        assertFalse(timeFormat.IsTimeValid("12:30:45"));
     }
 
     @Test
     @DisplayName("Testing for 24:00 as midnight | false") // range goes from 00 to 23 NOT 24
-    void testingIncorrect24Clock(){Assertions.assertFalse(TimeFormat.IsTimeValid("24:00"));}
+    void testingIncorrect24Clock(){assertFalse(timeFormat.IsTimeValid("24:00"));}
 
     @Test
     @DisplayName("Testing for 00:00 as midnight | true")
-    void testingCorrectClock2(){Assertions.assertTrue(TimeFormat.IsTimeValid("00:00"));}
+    void testingCorrectClock2(){assertTrue(timeFormat.IsTimeValid("00:00"));}
 
     @Test
-    void testingnull(){Assertions.assertFalse(TimeFormat.IsTimeValid(""));}
+    void testingNull(){assertFalse(timeFormat.IsTimeValid(""));}
 }
 
